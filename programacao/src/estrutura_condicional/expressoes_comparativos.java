cda728d6572333249d3e0154bd50fb65261c04d0
Vinícius Silva <xdenik13@gmail.com>
2022-08-23 10:59:28 -0300

    mod. estrutura condicional
    criando o package e a class expressões comparativas

---
+++ programacao/src/estrutura_condicional/expressoes_comparativos.java
@@ -0,0 +1,24 @@
+package estrutura_condicional;
+
+public class expressoes_comparativos {
+	
+	public static void main(String[] args) {
+		
+		/* 
+		 
+		 OPERADORES COMPARATIVOS:
+		 
+		 OPERADOR:			SIGNIFICADO:
+		 
+		 >					maior
+		 <					menor
+		 >=					maior ou igual
+		 <=					menor ou igual
+		 ==  				igual
+		 !=					diferente
+		 
+		 
+		 */
+	}
+
+}
