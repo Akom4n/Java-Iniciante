2e3e62dcc5cf7422ea97eed112c96c5e8da34c1e
Vinícius Silva <xdenik13@gmail.com>
2022-08-23 22:03:44 -0300

    demonstração

---
+++ programacao/src/teste/testando.java
@@ -0,0 +1,20 @@
+package teste;
+
+public class testando {
+	
+	public static void main(String[] args) {
+		
+		int x, b;
+		
+		x = 5;
+		b = 6;
+		
+		if (x < b) {
+			System.out.println("com toda clara certeza ne meu mano");
+		}
+		else if(b < x) {
+			System.out.println("clramente nãaao né");
+		}
+	}
+
+}
