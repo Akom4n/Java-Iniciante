405a9cfce6278d8cb0f441827ceb5cdae13a36ca
Vinícius Silva <xdenik13@gmail.com>
2022-08-24 14:09:11 -0300

    teste de mesa

---
+++ programacao/src/estrutura_repetitiva/teste_de_mesa.java
@@ -0,0 +1,17 @@
+package estrutura_repetitiva;
+
+public class teste_de_mesa {
+	
+	public static void main(String[] args) {
+		
+		int x = 0;
+		int y = 4;
+		
+		while (x < 3) {
+			y = y + 2;
+			x = x + 1;
+			System.out.println(x + "-" + y);
+		}
+	}
+
+}
