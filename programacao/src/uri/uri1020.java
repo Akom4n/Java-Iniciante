abe68fa36759830c4b3bf75d12f5ef41bd333e40
Vinícius Silva <xdenik13@gmail.com>
2022-08-25 17:18:49 -0300

    aulas praticas

--- programacao/src/uri/uri1020.java
+++ programacao/src/uri/uri1020.java
@@ -1,5 +1,30 @@
 package uri;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class uri1020 {
+	
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int N, anos, meses, dias, resto;
+		
+		N = sc.nextInt();
+		
+		anos = N / 365;
+		resto = N % 365;
+		meses = resto / 30;
+		dias = resto % 30;
+		
+		
+		System.out.println(anos + " ano(s)");
+		System.out.println(meses + " mes(es)");
+		System.out.println(dias + " dia(s)");
+		
+		
+		sc.close();
+	}
 
 }
---
+++ programacao/src/uri/uri1078.java
@@ -0,0 +1,23 @@
+package uri;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1078 {
+	
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		
+		for (int i=1; i<=10; i++) {
+			int produto = i * N;
+			System.out.println(i + " x " + N + " = " + produto);
+		}
+		
+		sc.close();
+	}
+
+}
